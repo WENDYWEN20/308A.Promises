5a8534b2d23ce837baa97f409baeac0ea2a97524
wendywen20 <wenyimei6699@gmail.com>
2024-08-26 14:55:39 -0400

    promises

---
+++ index.js
@@ -0,0 +1,14 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+  console.log(db1)
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+}
+
+
+getUserData()
